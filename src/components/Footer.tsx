fcb036def81863c6ba5a691b00dd1002fc45849f
kuladeepchowdary <kuladeep.swe@gmail.com>
2025-10-15 15:26:50 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-400">
         <div className="flex justify-center space-x-6 mb-4">
           <a href="https://github.com/KuladeepAIKTSPL/DevOpsForAll" className="hover:text-neon-blue transition-colors"><Github /></a>
-          <a href="#" className="hover:text-neon-blue transition-colors"><Linkedin /></a>
+          <a href="https://www.linkedin.com/company/aikshetra-tech-solutions-private-limited/posts/?feedView=all" className="hover:text-neon-blue transition-colors"><Linkedin /></a>
           <a href="#" className="hover:text-neon-blue transition-colors"><Twitter /></a>
         </div>
         <p>&copy; {new Date().getFullYear()} DevOps for Everyone. All Rights Reserved.</p>
